80117918136b9e8a4f3ae846bf067ae98d40b3b1
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-09-30 20:03:47 +0500

    added circle-ci

--- src/test/java/com/guru/recipe/controller/IndexControllerTest.java
+++ src/test/java/com/guru/recipe/controller/IndexControllerTest.java
@@ -1,14 +1,19 @@
 package com.guru.recipe.controller;
 
+import com.guru.recipe.model.Recipe;
 import com.guru.recipe.service.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
-import static org.junit.Assert.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
 
 public class IndexControllerTest {
 
@@ -29,9 +34,22 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() {
 
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setDescription("A");
+        recipeSet.add(recipe);
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+        //when
         String viewName = indexController.getIndexPage(model);
         assertEquals("index", viewName);
         Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
-        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.eq("recipes"),Mockito.anySet());
-}
+        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
+    }
 }
