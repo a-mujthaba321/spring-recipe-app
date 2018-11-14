31b1bb086543c8a1bc624084d73f5afcffe252f9
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-11-14 20:49:07 +0500

    Added recipe show for single recipes

--- src/main/java/com/guru/recipe/service/RecipeServiceImpl.java
+++ src/main/java/com/guru/recipe/service/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -20,8 +21,19 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        System.out.println("recipe");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepo.findAll().iterator().forEachRemaining(recipes::add);
         return recipes;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipe = recipeRepo.findById(id);
+        if(!recipe.isPresent()){
+            throw new RuntimeException("Recipe Not Found");
+        }
+
+        return recipe.get();
     }
 }
---
+++ src/test/java/com/guru/recipe/controller/RecipeControllerTest.java
@@ -0,0 +1,49 @@
+package com.guru.recipe.controller;
+
+import com.guru.recipe.model.Recipe;
+import com.guru.recipe.service.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+
+    @Before
+    public void setUp() throws Exception{
+        MockitoAnnotations.initMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void showRecipeById() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/1/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+}
